5e96ef0f69eeceaabd1153034984da2c4f615e32
ChachyDev <shalomadecoolboy@outlook.com>
2021-05-14 21:37:09 +0100

    Remove try catch block to make the game crash.

--- src/main/java/org/bookmc/loader/BookModLoader.java
+++ src/main/java/org/bookmc/loader/BookModLoader.java
@@ -3,6 +3,7 @@ package org.bookmc.loader;
 import org.bookmc.loader.exception.MissingDependencyException;
 import org.bookmc.loader.vessel.ModVessel;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,26 +11,21 @@ public class BookModLoader {
     private static final List<ModVessel> loaded = new ArrayList<>();
 
     public static void load() {
-        try {
-            for (ModVessel vessel : Loader.getModVessels()) {
-                if (loaded.contains(vessel)) {
-                    continue;
-                }
-
-                loadDependencies(vessel);
-
-                // In the chances of someone for some stupid reason decided to add their own mod as a dependency
-                if (!loaded.contains(vessel)) {
-                    load(vessel);
-                }
-
+        for (ModVessel vessel : Loader.getModVessels()) {
+            if (loaded.contains(vessel)) {
+                continue;
             }
-        } catch (Throwable t) {
-            t.printStackTrace();
+
+            loadDependencies(vessel);
+
+            // In the chances of someone for some stupid reason decided to add their own mod as a dependency
+            if (!loaded.contains(vessel)) {
+                load(vessel);
+            }
         }
     }
 
-    private static void loadDependencies(ModVessel vessel) throws Throwable {
+    private static void loadDependencies(ModVessel vessel) {
         for (String dependency : vessel.getDependencies()) {
             // I believe this will become O(n^2), is there a better way to this?
             boolean isFound = false;
@@ -48,18 +44,32 @@ public class BookModLoader {
         }
     }
 
-    private static void load(ModVessel vessel) throws Throwable {
+    private static void load(ModVessel vessel) {
         String[] split = vessel.getEntrypoint().split("::");
 
-        Class<?> entryClass;
+        Class<?> entryClass = null;
 
         if (vessel.getClassLoader() != null) {
-            entryClass = Class.forName(split[0], true, vessel.getClassLoader());
+            try {
+                entryClass = Class.forName(split[0], true, vessel.getClassLoader());
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
         } else {
-            entryClass = Class.forName(split[0]);
+            try {
+                entryClass = Class.forName(split[0]);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
         }
 
         loaded.add(vessel);
-        entryClass.getDeclaredMethod(split[1]).invoke(entryClass.getConstructor().newInstance());
+        try {
+            if (entryClass != null) {
+                entryClass.getDeclaredMethod(split[1]).invoke(entryClass.getConstructor().newInstance());
+            }
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | InstantiationException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/bookmc/loader/exception/MissingDependencyException.java
+++ src/main/java/org/bookmc/loader/exception/MissingDependencyException.java
@@ -2,6 +2,6 @@ package org.bookmc.loader.exception;
 
 public class MissingDependencyException extends IllegalStateException {
     public MissingDependencyException(String dependency) {
-        super(String.format("The dependency %s could not be found! Please make sure it is located in the mods folder", dependency));
+        super(String.format("The dependency \"%s\" could not be found! Please make sure it is located in the mods folder", dependency));
     }
 }
